2b1da20e8e54d5d8014e89b7a5c5ffdbdd72574e
unknown <dineshigdd@gmail.com>
2022-03-03 21:59:06 -0800

    change visibility property back to display property

--- header/header.js
+++ header/header.js
@@ -2,14 +2,14 @@ let isNavDisplay = false;
 
 document.querySelector(".menu-toggle").onclick = () => {
   if( !isNavDisplay ){
-    document.querySelector('#site-navigation').style.visibility = 'visible';
-   /*  document.querySelector("#primary-menu").style.display = 'grid';
-    document.querySelector("#menu-header-menu").style.display = 'flex'; */
+  
+    document.querySelector("#primary-menu").style.display = 'grid';
+    document.querySelector("#menu-header-menu").style.display = 'flex';
     isNavDisplay = true;
 }else{
-  document.querySelector('#site-navigation').style.visibility = 'hidden';
-  /* document.querySelector("#primary-menu").style.display = 'none';
-  document.querySelector("#menu-header-menu").style.display = 'none'; */
+  
+  document.querySelector("#primary-menu").style.display = 'none';
+  document.querySelector("#menu-header-menu").style.display = 'none';
   isNavDisplay = false;
 }
 };
