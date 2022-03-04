127b1393b43fe8d333797202b126b6646e5e6fd4
unknown <dineshigdd@gmail.com>
2022-03-03 21:52:22 -0800

    comment style.display and add style.visibility

--- header/header.js
+++ header/header.js
@@ -2,12 +2,14 @@ let isNavDisplay = false;
 
 document.querySelector(".menu-toggle").onclick = () => {
   if( !isNavDisplay ){
-    document.querySelector("#primary-menu").style.display = 'grid';
-    document.querySelector("#menu-header-menu").style.display = 'flex';
+    document.querySelector('#site-navigation').style.visibility = 'visible';
+   /*  document.querySelector("#primary-menu").style.display = 'grid';
+    document.querySelector("#menu-header-menu").style.display = 'flex'; */
     isNavDisplay = true;
 }else{
-  document.querySelector("#primary-menu").style.display = 'none';
-  document.querySelector("#menu-header-menu").style.display = 'none';
+  document.querySelector('#site-navigation').style.visibility = 'hidden';
+  /* document.querySelector("#primary-menu").style.display = 'none';
+  document.querySelector("#menu-header-menu").style.display = 'none'; */
   isNavDisplay = false;
 }
 };
