918a9a731937238e6a864b0894fbd149e8bf1010
unknown <dineshigdd@gmail.com>
2022-03-03 21:44:27 -0800

    add styles and add fx to hamburgerIcon

--- header/header.js
+++ header/header.js
@@ -1,10 +1,16 @@
+let isNavDisplay = false;
 
-
-document.querySelector(".menu-toggle").onclick = function() {myFunction()};
-
-function myFunction() {
-  document.querySelector("#primary-menu").style.display = 'grid';
-  // document.getElementById("secondary-menu").style.display = 'block';
+document.querySelector(".menu-toggle").onclick = () => {
+  if( !isNavDisplay ){
+    document.querySelector("#primary-menu").style.display = 'grid';
+    document.querySelector("#menu-header-menu").style.display = 'flex';
+    isNavDisplay = true;
+}else{
+  document.querySelector("#primary-menu").style.display = 'none';
+  document.querySelector("#menu-header-menu").style.display = 'none';
+  isNavDisplay = false;
 }
+};
+
 
 
